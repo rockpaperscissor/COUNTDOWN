92406bc8cca05c3a40f5a286a2d902b449a58f13
GAYATHRY P VASUDEVAN <84952234+rockpaperscissor@users.noreply.github.com>
2023-08-13 20:20:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const endDate = "6 August 2023 10:32:40 PM"
+document.getElementById("end-date").innerText = endDate;
+const inputs= document.querySelectorAll("input")
+function clock() {
+  const end = new Date(endDate);
+  const now = new Date();
+
+
+  const diff = Math.round((end - now) / 1000);
+
+  if(diff <0) return;
+  inputs[1].value=(Math.floor(diff/3600)%24);
+  inputs[0].value = Math.round(diff/3600/24);
+  inputs[2].value=Math.floor((diff/60)%60);
+  inputs[3].value=Math.floor((diff)%60);
+  
+ 
+}
+clock()
+setInterval(
+  ()=>{
+    clock()
+  },1000
+)
